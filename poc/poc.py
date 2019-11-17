bd40cee031c95c2fb7c51f2012708419c3b9586c
Evan Goode <mail@evangoo.de>
2019-11-17 15:44:11 -0500

    2019-11-17 tests

--- poc/poc.py
+++ poc/poc.py
@@ -10,17 +10,18 @@ import scipy.io.wavfile
 
 fs, raw_data = scipy.io.wavfile.read(sys.argv[1])
 print(f"Detected sampling frequency: {fs}Hz")
+print(f"Found {raw_data.shape[1]} channels.")
 channels = raw_data.T
 
 c = 343.    # speed of sound
 nfft = 256  # FFT size
 freq_range = [300, 3500]
 
-mic_positions = pra.circular_2D_array(center=(0, 0), M=2, phi0=0, radius=(1 / 2))
+mic_positions = pra.circular_2D_array(center=(0, 0), M=3, phi0=0, radius=0.19685)
 
 X = np.array([pra.stft(channel, nfft, nfft // 2, transform=np.fft.rfft).T for channel in channels])
 
-doa = pra.doa.algorithms["SRP"](mic_positions, fs, nfft, c=c, num_src=1, max_four=4)
+doa = pra.doa.algorithms["MUSIC"](mic_positions, fs, nfft, c=c, num_src=1, max_four=4)
 doa.locate_sources(X, freq_range=freq_range)
 
 spatial_resp = doa.grid.values
@@ -37,4 +38,9 @@ c_dirty_img = np.r_[spatial_resp, spatial_resp[0]]
 ax.plot(c_phi_plt, base + height * c_dirty_img, linewidth=3,
         alpha=0.55, linestyle='-',
         label="spatial spectrum")
-plt.show()
+
+if len(sys.argv) > 2:
+    plt.savefig(sys.argv[2])
+    print(f"Saved to {sys.argv[2]}")
+else:
+    plt.show()
