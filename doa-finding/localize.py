71191cd8015e419f9e3d72cf95ea4c0a993795cf
Evan Goode <mail@evangoo.de>
2019-12-03 18:34:11 -0500

    chick-fil-a and mario sounds

--- doa-finding/localize.py
+++ doa-finding/localize.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import math
 import sys
 
 import matplotlib
@@ -13,19 +14,24 @@ import IPython
 fs, raw_data = scipy.io.wavfile.read(sys.argv[1])
 print(f"Detected sampling frequency: {fs}Hz")
 print(f"Found {raw_data.shape[1]} channels.")
-channels = raw_data.T
+raw_channels = raw_data.T
+channels = np.copy(raw_channels)
+# channels[1] = raw_channels[2]
+# channels[2] = raw_channels[1]
+# channels = raw_data.T[::-1]
 
-c = 343.    # speed of sound
+c = 13503.94    # speed of sound in inches/second
 nfft = 256  # FFT size
 freq_range = [300, 3500]
 
-mic_positions = pra.circular_2D_array(center=(0, 0), M=3, phi0=0, radius=0.19685)
+mic_positions = pra.circular_2D_array(center=(0, 0), M=3, phi0=-math.pi / 6, radius=8.66)
+print(mic_positions)
 
 X = np.array([pra.stft(channel, nfft, nfft // 2, transform=np.fft.rfft).T for channel in channels])
 
 doa = pra.doa.algorithms["MUSIC"](mic_positions, fs, nfft, c=c, num_src=1, max_four=4)
 doa.locate_sources(X, freq_range=freq_range)
-IPython.embed()
+# IPython.embed()
 
 spatial_resp = doa.grid.values
 phi_plt = doa.grid.azimuth
